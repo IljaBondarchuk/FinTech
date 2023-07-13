1bf41a46a0c8c9e560d16097f148f5da18557454
bondarch1 <ext-illia.bondarchuk@here.com>
2023-07-13 16:23:10 +0300

    task 3 refactor

--- src/test/java/com/example/FinTech/service/AccountServiceImplTest.java
+++ src/test/java/com/example/FinTech/service/AccountServiceImplTest.java
@@ -4,6 +4,7 @@ import com.example.FinTech.entity.Account;
 import com.example.FinTech.exception.AccountNotFoundException;
 import com.example.FinTech.exception.DuplicateDataException;
 import com.example.FinTech.repository.AccountRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,14 +16,15 @@ import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness =  Strictness.LENIENT)
@@ -38,7 +40,7 @@ public class AccountServiceImplTest {
     @BeforeEach
     public void setup(){
         account = Account.builder()
-                .id(1L)
+                .id(0L)
                 .firstName("Serious")
                 .lastName("Sam")
                 .identifier(11111111L)
@@ -46,7 +48,7 @@ public class AccountServiceImplTest {
                 .annualIncome(new BigDecimal(10000))
                 .creditLimit(new BigDecimal(0))
                 .depositBalance(new BigDecimal(1000))
-                .availableBalance(new BigDecimal(100))
+                .availableBalance(new BigDecimal(0))
                 .creditBalance(new BigDecimal(100))
                 .build();
     }
@@ -114,32 +116,96 @@ public void givenAccountObject_whenSaveAccount_thenReturnAccountObjectWithCredit
     given(accountRepository.findById(account.getId()))
             .willReturn(Optional.empty());
     given(accountRepository.save(account)).willReturn(account);
-    System.out.println(accountRepository);
-    System.out.println(accountService);
 
     //when - action or the behaviour that we are going testt
     Account savedAccount = accountService.save(account);
-    System.out.println(savedAccount);
 
     //then verify the output
     assertThat(savedAccount.getAvailableBalance()).isZero();
     assertThat(savedAccount.getCreditBalance()).isZero();
 }
 
-@DisplayName("Junit test for update method should return the same available balance")
+
+
+  //  @Disabled
+    @DisplayName("Junit test for update method should return the same available balance and changes another value")
     @Test
     public void givenAccountObject_whenUpdateAccount_thenReturnAccountWithTheSameAvailableBalance(){
+        Account newAccount = Account.builder()
+                .id(0L)
+                .firstName("Serious")
+                .lastName("Sam")
+                .identifier(11111111L)
+                .passportNumber(222222L)
+                .annualIncome(new BigDecimal(999))
+                .creditLimit(new BigDecimal(0))
+                .depositBalance(new BigDecimal(1000))
+                .availableBalance(new BigDecimal(10000))
+                .creditBalance(new BigDecimal(100))
+                .build();
 
-    given(accountRepository.findById(account.getId()))
-            .willReturn(account);
+    given(accountRepository.findById(newAccount.getId())).willReturn(Optional.of(account));
+    given(accountRepository.save(newAccount)).willReturn(newAccount);
+
+   Account updatedAccount = accountService.update(newAccount);
 
 
-    Account savedAccount = accountService.save(account);
-    Account updatedAccount = accountService.update(accountUpdate);
-
-    assertThat(savedAccount.getAvailableBalance()).isEqualTo(updatedAccount.getAvailableBalance());
+   // assertThat(updatedAccount).isNotNull();
+  assertThat(updatedAccount.getAvailableBalance()).isEqualTo(new BigDecimal(0));
+  assertThat(updatedAccount.getAnnualIncome()).isEqualTo(new BigDecimal(999));
 }
+    @DisplayName("JUnit test for delete Account and should return Id of deleting account ")
+    @Test
+    public void givenAccountObject_whenDeleteAccount_thenReturnIdOfDeletedAccount(){
+        //given precondition or setup
+        given(accountRepository.findById(account.getId()))
+                .willReturn(Optional.ofNullable(account));
 
+        //when - action or the behaviour that we are going test
+        accountService.deleteById(account.getId());
 
+        //then verify the output
+        verify(accountRepository,times(1)).deleteById(account.getId());
+    }
 
+    @DisplayName("JUnit test for findAll Accounts and should return list of accounts")
+    @Test
+    public void givenListOfAccountObjects_whenFindAllAccount_thenReturnListOfAccounts(){
+        //given precondition or setup
+        Account account1 = Account.builder()
+                .id(1L)
+                .firstName("Abcd")
+                .lastName("Dcba")
+                .identifier(11111133L)
+                .passportNumber(222211L)
+                .annualIncome(new BigDecimal(10000))
+                .creditLimit(new BigDecimal(0))
+                .depositBalance(new BigDecimal(1000))
+                .availableBalance(new BigDecimal(0))
+                .creditBalance(new BigDecimal(100))
+                .build();
+
+        Account account2 = Account.builder()
+                .id(2L)
+                .firstName("Qwerty")
+                .lastName("Asdfg")
+                .identifier(11111134L)
+                .passportNumber(222212L)
+                .annualIncome(new BigDecimal(10000))
+                .creditLimit(new BigDecimal(0))
+                .depositBalance(new BigDecimal(1000))
+                .availableBalance(new BigDecimal(0))
+                .creditBalance(new BigDecimal(100))
+                .build();
+
+        given(accountRepository.findAll())
+                .willReturn(List.of(account,account1,account2));
+
+        //when - action or the behaviour that we are going test
+        List <Account> accountList = accountService.findAll();
+
+        //then verify the output
+        assertThat(accountList).isNotNull();
+        assertThat(accountList.size()).isEqualTo(3);
+    }
 }
